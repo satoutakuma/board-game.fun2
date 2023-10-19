f704a7470ae659d582ef38d6eb72f3459552b6cc
EC2 Default User <ec2-user@ip-172-31-37-157.ap-northeast-1.compute.internal>
2023-10-19 12:15:13 +0000

    customer show

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,26 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = current_customer
+    @user = Customer.find(params[:id])
+    @games = @user.games
   end
 
   def edit
   end
 
   def update
+  end
+  
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :profile_image, :introduction, :is_active)
+  end
+
+  def is_matching_login_customer
+    customer = Customer.find(params[:id])
+    unless customer.id == current_customer.id
+      redirect_to customer_path(current_customer.id)
+    end
   end
 end
