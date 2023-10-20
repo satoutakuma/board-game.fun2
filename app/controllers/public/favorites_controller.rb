46e65298cfb03e620d02bc05f37b24e7a21e2877
EC2 Default User <ec2-user@ip-172-31-37-157.ap-northeast-1.compute.internal>
2023-10-20 13:50:57 +0000

    favorite

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,7 +1,19 @@
 class Public::FavoritesController < ApplicationController
   def create
+    @game = Game.find(params[:game_id])
+    @favorite = Favorite.new(customer_id: current_customer.id, game_id: params[:game_id])
+    @favorite.save!
   end
 
   def destroy
+    @game = Game.find(params[:game_id])
+    @favorite = Favorite.find_by(customer_id: current_customer.id, game_id: params[:game_id])
+    @favorite.destroy!
+  end
+  
+  private
+
+  def set_game
+    @game = Game.find(params[:game_id])
   end
 end
--- app/controllers/public/game_comments_controller.rb
+++ app/controllers/public/game_comments_controller.rb
@@ -1,7 +1,27 @@
 class Public::GameCommentsController < ApplicationController
   def create
+    game = Game.find(params[:game_id])
+    if current_customer
+      comment = current_customer.game_comments.new(game_comment_params)
+      comment.game_id = game.id
+      if comment.save
+        redirect_to game_path(params[:game_id])
+      else
+        redirect_to new_customer_session_path
+      end
+    else
+      redirect_to new_customer_registration_path
+    end
   end
 
   def destroy
+    GameComment.find(params[:id]).destroy
+    redirect_to game_path(params[:game_id])
+  end
+  
+  private
+
+  def game_comment_params
+    params.require(:game_comment).permit(:comment)
   end
 end
