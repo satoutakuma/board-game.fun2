d203edd69802ca76c0ac0bc6f95d7f922411516e
EC2 Default User <ec2-user@ip-172-31-37-157.ap-northeast-1.compute.internal>
2023-10-23 11:49:42 +0000

    games controller

--- app/controllers/public/game_reply_controller.rb
+++ app/controllers/public/game_reply_controller.rb
@@ -1,7 +1,29 @@
 class Public::GameReplyController < ApplicationController
   def create
+    @game = Game.find(params[:game_id])
+    @parent_comment = GameComment.find(params[:game_comment_id])
+    @reply_comment = @parent_comment.replies.new(game_reply_params)
+    @reply_comment.customer = current_customer
+
+    if @reply_comment.save!
+      redirect_to game_path(@game)
+    else
+      @new = Game.new
+      @user = current_customer
+      @users = @game.customer
+      @game_comment = GameComment.new
+      render "show"
+    end
   end
 
   def destroy
+    @reply = GameReply.find(params[:id])
+  	@reply.destroy
+  	redirect_to game_path(params[:game_id])
+  end
+  
+  private
+  def game_reply_params
+    params.require(:game_reply).permit(:comment)
   end
 end
--- app/controllers/public/games_controller.rb
+++ app/controllers/public/games_controller.rb
@@ -1,11 +1,29 @@
 class Public::GamesController < ApplicationController
   def new
+    @game = Game.new
   end
 
   def create
+    @game = Game.new(game_params)
+    @game.customer_id = current_customer.id
+    if @game.save
+      flash[:notice] = "game was successfully created."
+      redirect_to game_path(@game)
+    else
+      @games = Game.all
+      @customer = current_customer
+      render :index
+    end
   end
 
   def index
+    @games = Game.joins(:customer).where(customers: { is_active: true })
+    @user = current_customer
+    if params[:min_players].present? || params[:max_players].present?
+      @games = Game.where("min_players >= ? AND max_players <= ?", params[:min_players].to_i, params[:max_players].to_i).order(min_players: :asc)
+    else
+      @games = Game.all
+    end
   end
 
   def show
